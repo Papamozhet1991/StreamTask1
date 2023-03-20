8d116a7e6d6ef5b46ea30217523cecb24d45c87b
Artyom Skovorodko <papamozhet1991@yandex.ru>
2023-03-20 18:49:08 +0500

    Updating

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        List<Integer> intList2 = new ArrayList<>(intList);
-        Iterator<Integer> iterator = intList2.iterator();
+        List<Integer> intList2 = new ArrayList<>();
 
-        while(iterator.hasNext()){
-            Integer local = iterator.next();
-            if (local <= 0){
-                iterator.remove();
-            }
-        }
-
-        for (int i=0; i<intList2.size();i++){
-            int even = intList2.get(i)%2;
-            if (even != 0) {
-                intList2.remove(i);
-                i--;
+        for (Integer local : intList){
+            if (local > 0 && local % 2 == 0){
+                intList2.add(local);
             }
         }
 
